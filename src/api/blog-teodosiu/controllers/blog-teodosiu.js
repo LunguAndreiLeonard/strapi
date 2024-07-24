dfa52b56d88a0da9ad2e33d62f7d96a7f188201a
andruleo99 <andruleo99@gmail.com>
2024-07-24 15:28:09 +0300

    update teodosiu

---
+++ src/api/blog-teodosiu/controllers/blog-teodosiu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-teodosiu controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::blog-teodosiu.blog-teodosiu');
---
+++ src/api/blog-teodosiu/services/blog-teodosiu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * blog-teodosiu service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::blog-teodosiu.blog-teodosiu');
